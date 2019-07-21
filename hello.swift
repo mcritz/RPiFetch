7ccbf2747c676b4e483919c6486775d4d7e79ba5
Michael Critz <inquiries@michaelcritz.com>
2019-07-21 09:22:27 -0700

    Updated weather excludes

--- hello.swift
+++ hello.swift
@@ -5,11 +5,11 @@ import Foundation
 print("Daily Pi! Powered by DarkSky")
 
 guard let wxApiKey = ProcessInfo.processInfo.environment["DARKSKYAPIKEY"] else {
-    fatalError("Must set DARKSKYAPIKEY environment variable. Get an API key at https://darksky.net/dev")
+    fatalError("Must set DARKSKYAPIKEY environment variable. Get an API key at https://darksky.net/poweredby")
 }
 
 
-guard let url = URL(string: "https://api.darksky.net/forecast/\(wxApiKey)/37.8267,-122.4233") else {
+guard let url = URL(string: "https://api.darksky.net/forecast/\(wxApiKey)/37.8267,-122.4233?exclude=minutely,hourly,daily,alerts,flags") else {
     fatalError("Could not create a valid url for \(wxApiKey)")
 }
 
@@ -19,3 +19,4 @@ guard let contents = try? String(contentsOf: url) else {
 }
 
 print(contents)
+
