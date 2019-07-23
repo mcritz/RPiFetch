1d7430263f79b585ed3f03174632f4c2be984bb0
Michael Critz <inquiries@michaelcritz.com>
2019-07-22 20:19:32 -0700

    simplifiy display messsage

--- Assets/display_text.py
+++ Assets/display_text.py
@@ -3,6 +3,9 @@ from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 
+wxfile = open("/home/pi/Documents/current-wx.txt")
+currentWX = wxfile.read()
+
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.BLACK)
 
@@ -13,7 +16,8 @@ draw.rectangle([(0, 0), img.size], fill=inky_display.RED)
 
 font = ImageFont.truetype(FredokaOne, 22)
 
-message = sys.argv[1]
+# message = sys.argv[1]
+message = currentWX
 
 print("will display: ")
 print(message)
@@ -23,7 +27,7 @@ w, h = font.getsize(message)
 x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
 
-draw.text((x,y), message, inky_display.WHITE, font)
+draw.text((0,0), message, inky_display.WHITE, font)
 img.save("image.png")
 inky_display.set_image(img)
 inky_display.show()
